4fb3a33d14ed307dda96d20ad16a2a7877a7d72b
wzamlynny <wzamlynny@gmail.com>
2017-01-20 17:25:59 -0500

    start apply

--- apply/families.php
+++ apply/families.php
@@ -32,7 +32,49 @@
     <!--=========== END COURSE BANNER SECTION ================-->
     
     <!--=========== BEGIN COURSE BANNER SECTION ================-->
-    
+    <section id="application">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12">
+            <div class="title_area">
+              <h2 class="title_two">Family Registration</h2>
+              <span></span>
+              <p>Thank you very much for contacting Educate the Future. Please take a moment to complete our Family Registration form so that we may tailor your experience to better serve your child. We look forward to working with your child and ensuring a brighter future!</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12">
+            <div class="application_form wow fadeInLeft">
+            <form>
+              <h3>Parental Guardian(s)</h3>
+                <input type="text"  class="wp-form-control wpcf7-text" placeholder="First Name" name="firstName1"></input>
+                <input type="text"  class="wp-form-control wpcf7-text" placeholder="LastName" name="lastName1"></input>
+                </br>
+                <input type="text"  class="wp-form-control wpcf7-text" placeholder="First Name" name="firstName2"></input>
+                <input type="text"  class="wp-form-control wpcf7-text" placeholder="LastName" name="lastName2"></input>
+                </br>
+
+                <span>Address</span>
+                <span>Street</span> <input type="text" name="streetAddress"></input>
+                <span>City</span> <input type="text" name="cityAddress"></input>
+                <span>State</span> 
+                  <select name="stateAddress">
+                    <option value="NJ">New Jersey</option>
+                  </select>
+
+                <span>Phone Number</span> <input type="text" name="phoneNumber"></input>
+                <span>e-mail Address</span> <input type="text" name="email"></input>
+
+              <h3>Student</h3>
+            </form>
+            </div>
+          </div>
+        <div>
+
+
+      </div>
+    </section>
     <!--=========== END COURSE BANNER SECTION ================-->
     
     <!--=========== BEGIN FOOTER SECTION ================-->
