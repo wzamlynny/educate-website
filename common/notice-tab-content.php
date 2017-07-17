acf55055111438c8362d68d3278bb8a40a439b98
Weronika Zamlynny <wzamlynny@gmail.com>
2017-07-16 21:06:20 -0400

    fix links

--- common/notice-tab-content.php
+++ common/notice-tab-content.php
@@ -15,12 +15,12 @@
 <li>
 <div class="media">
         <div class="media-left">
-        <a href="events/getfeatured.php" class="news_img">
-            <img alt="img" src="img/events/getfeatured-20170716.jpg" class="media-object">
+        <a href=<?php echo "'" . $root . "events/getfeatured.php'"; ?> class="news_img">
+            <img alt="img" src=<?php echo "'" . $root . "img/events/getfeatured-20170716.jpg'"; ?> class="media-object">
         </a>
         </div>
         <div class="media-body">
-        <a href="events/getfeatured.php">Get Featured on Educate the Future</a>
+        <a href=<?php echo "'" . $root . "events/getfeatured.php'"; ?>>Get Featured on Educate the Future</a>
         <span class="feed_date">16 July 2017</span>
         </div>
     </div>
@@ -28,12 +28,12 @@
 <li>
 <div class="media">
         <div class="media-left">
-        <a href="events/call-20170722.php" class="news_img">
-            <img alt="img" src="img/educate_logo.png" class="media-object">
+        <a href=<?php echo "'" . $root . "events/call-20170722.php'"; ?> class="news_img">
+            <img alt="img" src=<?php echo "'" . $root . "img/educate_logo.png'"; ?> class="media-object">
         </a>
         </div>
         <div class="media-body">
-        <a href="events/call-20170722.php">Live Conference Call - What's Next?</a>
+        <a href=<?php echo "'" . $root . "events/call-20170722.php'"; ?>>Live Conference Call - What's Next?</a>
         <span class="feed_date">22 July 2017</span>
         </div>
     </div>
